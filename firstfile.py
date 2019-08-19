8ea3eb70c01abf1ca2ab03223d4b7e9e5b7ddfc6
cwkreid19141 <student+19141@codingwithkids.com>
2019-08-19 14:32:57 -0700

    Look at my new viola for 1 dollar

--- firstfile.py
+++ firstfile.py
@@ -5,8 +5,17 @@ def main():
 def urmom():
     print("Thanos")
 
+
+def viola():
+    print("O\|/")
+
+
 if __name__ == "__main__":
     main()
 
 if __name__ == "__urmom__":
     urmom()
+
+
+if __name__ == "__viola__":
+    viola()
